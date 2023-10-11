2efe09de6c07d6c8f9142b47a8c9a363d914fd84
Laksito Pamilih <laksito@protonmail.com>
2023-10-11 14:10:33 +0700

    add put and delete envelope

--- server.js
+++ server.js
@@ -16,6 +16,38 @@ app.get('/envelopes', (req, res) => {
   res.send(envelopes)
 })
 
+app.get('/envelopes/:id', (req, res) => {
+  console.log(req.params)
+  const id = +req.params.id
+  const found = envelopes.find(item => item.id === id)
+
+  if (found) {
+    res.send(found)
+  } else {
+    res.status(404).send()
+  }
+})
+
+app.put('/envelopes/:id', (req, res) => {
+  const id = +req.params.id
+  const foundIndex = envelopes.findIndex(item => item.id === id)
+
+  if (foundIndex > -1) {
+    const { title, budget } = req.body
+
+    const updated = { ...envelopes[foundIndex] }
+
+    if (title) updated.title = title
+    if (budget) updated.budget = budget
+
+    envelopes[foundIndex] = updated
+
+    res.status(200).send(envelopes[foundIndex])
+  } else {
+    res.status(404).send()
+  }
+})
+
 app.post('/envelopes', (req, res) => {
   const newEnvelope = {
     id: nextId++,
@@ -28,6 +60,17 @@ app.post('/envelopes', (req, res) => {
   res.status(201).send(envelopes[envelopes.length - 1])
 })
 
+app.delete('/envelopes/:id', (req, res) => {
+  const id = +req.params.id
+  const foundIndex = envelopes.findIndex(item => item.id === id)
+
+  if (foundIndex >= -1) {
+    envelopes.splice(foundIndex, 1)
+    res.status(204).send()
+  } else {
+    res.status(404).send()
+  }
+})
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
